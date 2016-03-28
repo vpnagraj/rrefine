e55d581f36b24434a91725155432fc53b26b1b38
VP Nagraj <vpnagraj@virginia.edu>
2016-03-28 10:20:32 -0400

    abstracted project id by name logic to refine_id in utils.R

--- R/refine_delete.R
+++ R/refine_delete.R
@@ -12,45 +12,7 @@
 #'
 refine_delete <- function(project.name = NULL, project.id = NULL) {
 
-    if (is.null(project.name) & is.null(project.id)){
-
-        stop("you must supply either a project name or project id")
-
-    }
-
-    if (!is.null(project.id)) {
-
-        project.id <- project.id
-
-    } else {
-
-        resp <- refine_metadata()
-
-        name <- NULL
-
-        id <- names(rlist::list.mapv(
-            rlist::list.filter(resp[["projects"]],
-                               name == project.name),
-            name))
-
-        if (length(id) == 1) {
-
-            project.id <- id
-
-        } else if (length(id) == 0) {
-
-            stop(paste0("there are no projects found named '",
-                        project.name,
-                        "' ... try adjusting the project.name argument or use project.id"))
-
-        } else {
-
-            stop(paste0("there are mulitple projects named '",
-                        project.name,
-                        "' ... try addding a project.id"))
-
-        }
-    }
+    project.id <- refine_id(project.name, project.id)
 
     x <- readline(prompt = "are you sure you want to delete this project? (Y/N): ")
 
--- R/utils.R
+++ R/utils.R
@@ -26,3 +26,52 @@ refine_metadata <- function() {
     )
 
 }
+
+#' helper function to get OpenRefine project.id by project.name
+#'
+#' @param project.name name of project to be exported or deleted
+#' @param project.id unique identifier for project to be exported or deleted
+#' @return unique id of project to be exported or deleted
+
+refine_id <- function(project.name, project.id) {
+
+    if (is.null(project.name) & is.null(project.id)){
+
+        stop("you must supply either a project name or project id")
+
+    }
+
+    if (!is.null(project.id)) {
+
+        project.id <- project.id
+
+    } else {
+
+        resp <- refine_metadata()
+
+        name <- NULL
+
+        id <- names(rlist::list.mapv(
+        rlist::list.filter(resp[["projects"]],
+            name == project.name),
+        name))
+
+        if (length(id) == 1) {
+
+            project.id <- id
+
+        } else if (length(id) == 0) {
+
+            stop(paste0("there are no projects found named '",
+            project.name,
+            "' ... try adjusting the project.name argument or use project.id"))
+
+        } else {
+
+            stop(paste0("there are mulitple projects named '",
+                project.name,
+                "' ... try addding a project.id"))
+
+        }
+    }
+}
