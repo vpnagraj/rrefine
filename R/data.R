25e5484e0c1864e35e4b1af228b81ed59298d4a2
VP Nagraj <vpnagraj@virginia.edu>
2016-04-05 17:36:16 -0400

    added sample clean dataset for vignette and new draft of vignette

--- R/data.R
+++ R/data.R
@@ -12,3 +12,19 @@
 #' @examples
 #' head(lateformeeting)
 "lateformeeting"
+
+#' a "clean" version of the lateformeeting sample data set
+#'
+#' This data is a simulated collection of dates, days of the week, numbers of hours slept and indicators of whether or not the subject was on time for work. All observations appearing in this data set are fictitious, and any resemblance to actual arrival times for work is purely coincidental.
+#'
+#' @format A data frame with 63 rows and 4 variables
+#' \itemize{
+#'   \item {date} {date of observation in POSIXct format}
+#'   \item {dotw} {day of the week in consistent format}
+#'   \item {hours.slept} {number of hours slept}
+#'   \item {on.time} {indicator of on-time arrival to work}
+#' }
+#' @examples
+#' head(lfm_clean)
+"lfm_clean"
+
--- data-raw/lateformeeting.R
+++ data-raw/lateformeeting.R
@@ -1,2 +1,5 @@
 lateformeeting <- read.csv("inst/extdata/lateformeeting.csv")
 save(lateformeeting, file = "data/lateformeeting.rda")
+
+lfm_clean <- read.csv("inst/extdata/lfm_cleanup.csv")
+save(lfm_clean, file = "data/lfm_clean.rda")
